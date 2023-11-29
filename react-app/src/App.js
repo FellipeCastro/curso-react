51d029e9e7652984bd75851185f574e050f791d3
FellipeCastro <fellipecastro27d@gmail.com>
2023-11-29 12:14:21 -0300

    Aula 04 finalizada

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,11 +1,28 @@
 import React from 'react';
+import css from './components/img/css-3.jpg';
 
 export default function App() {
-    return (
-      <>
-        <p>CFB Cursos</p>
-        <br/>
-        <p>Curso de React</p>
-      </>
-    )
+  const canal = () => {
+    return ('CFB Cursos')
+  }
+
+  function curso() {
+    return ("Curso de React")
+  }
+  
+  
+  return (
+    <>
+    <header>
+      <p>Canal: {canal()}</p>
+      <br/>
+      <p>{'Curso: ' + curso()}</p>
+    </header>
+    <section>
+      <img src={css}/>
+      <img src="/html-5.jpg"/>
+    </section>
+
+    </>
+  )
 }
