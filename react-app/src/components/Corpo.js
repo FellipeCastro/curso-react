2ea866687896c65add9fdd952cbc74a984db45fb
FellipeCastro <fellipecastro27d@gmail.com>
2023-11-30 16:55:00 -0300

    Aula 06 finalizada

--- react-app/src/components/Corpo.js
+++ react-app/src/components/Corpo.js
@@ -1,11 +1,22 @@
 import React from "react";
+import Dados from './Dados';
 
 export default function Corpo() {
+
+    const canal = 'CFB Cursos'
+    const youtube = 'youtube.com/cfbcursos'
+    const curso = 'React.js'
+
     return (
         <section>
             <h2>Curso de React</h2>
             <p>Se inscreva em nosso canal</p>
             <p>Ative o sininho e clique no joinha!</p>
+            <Dados 
+                canal= {canal}
+                youtube= {youtube}
+                curso= {curso}
+            />
         </section>
     )
 }
