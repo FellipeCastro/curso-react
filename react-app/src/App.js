c95f7efa2c26485dc470f056661c83394a2fd573
FellipeCastro <fellipecastro27d@gmail.com>
2023-12-01 11:13:20 -0300

    aula 08 finalizada

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,12 +1,21 @@
 import React from 'react';
-import Header from './components/Header';
-import Corpo from './components/Corpo';
+import './App.css';
 
-export default function App() {  
+export default function App() { 
+  
+    const textDestaque = {
+      color: '#00f',
+      fontSize: '3em'
+    }
+
   return (
     <>
-      <Header/>
-      <Corpo/>
+    <section className='caixa'>
+      <h1 style={{color: '#f00', fontSize: '5em'}}>CFB Cursos</h1>
+      <h2 style={textDestaque}>Curso de React</h2>
+      <p className='texto'>Se inscreva em nosso canal e nos siga nos instagram</p>      
+      <a href="https://www.youtube.com/cfbcursos" target='_blank'>CFB Cursos</a>
+    </section>
 
     </>
   )
