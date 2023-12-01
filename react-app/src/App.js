f84088c2bfef40cc6468294885b194f09ccb31d8
FellipeCastro <fellipecastro27d@gmail.com>
2023-12-01 11:23:22 -0300

    aula 09 finalizada

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import Relogio from './components/Relogio';
 
 export default function App() { 
   
@@ -11,12 +12,12 @@ export default function App() {
   return (
     <>
     <section className='caixa'>
+      <Relogio/>
       <h1 style={{color: '#f00', fontSize: '5em'}}>CFB Cursos</h1>
       <h2 style={textDestaque}>Curso de React</h2>
       <p className='texto'>Se inscreva em nosso canal e nos siga nos instagram</p>      
       <a href="https://www.youtube.com/cfbcursos" target='_blank'>CFB Cursos</a>
     </section>
-
     </>
   )
 }
