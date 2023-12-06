57adb0ca2fa0022b4accee0b2320de6cd08d6585
FellipeCastro <fellipecastro27d@gmail.com>
2023-12-06 18:40:03 -0300

    aula 21 finalizada

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,33 +1,14 @@
-import React, {useState, useEffect} from 'react';
-
+import React from 'react';
 
 import './App.css';
 
+import Classe from './components/Classe';
+
 export default function App() {
-  
-  const [nome, setNome] = useState()
-
-  const armazenar = (chave, valor) => {
-    localStorage.setItem(chave, valor)
-  }
-
-  const consultar = (chave) => {
-    alert(localStorage.getItem(chave))
-  }
-
-  const apagar = (chave) => {
-    localStorage.removeItem(chave)
-  }
-
   return (
     <>
-      <label htmlFor='nome'>Digite um Nome: </label>
-      <br/>
-      <input type='text' id='nome' value={nome} onChange={(e) => setNome(e.target.value)}/>
-      <br/>
-      <button onClick={() => armazenar('ls_nome', nome)}>Gravar nome</button>
-      <button onClick={() => consultar('ls_nome')}>Consultar nome</button>
-      <button onClick={() => apagar('ls_nome')}>Apagar nome</button>
+      <h1>Componentes de Classe</h1>
+      <Classe canal="CFB Cursos" curso="React"/>
     </>
   )
 }
