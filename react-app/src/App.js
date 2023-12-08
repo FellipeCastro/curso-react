3bb4bcc16af97aba6331fb71bb5842b7efc1474b
FellipeCastro <fellipecastro27d@gmail.com>
2023-12-08 16:50:19 -0300

    aula 26 finalizada

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,22 +1,13 @@
 import React, {useState} from 'react';
 
 import './App.css';
-
-import Classe from './components/Classe';
-import Carro from './components/Carro';
+import BaseClass from './components/BaseClasse';
 
 export default function App() {
 
-  const [carro, setCarro] = useState(true)
-  const remover = () => {
-    setCarro(!carro)
-  }
-
   return (
     <>
-      <h1>Componentes de Classe</h1>
-      {carro ? <Carro fator={10}/> : ""}      
-      <button onClick={() => remover()}>{carro ? "Remover Carro" : "Adicionar Carro"}</button>
+      <BaseClass/>
     </>
   )
 }
--- react-app/src/components/Canal.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-export default function Canal() {
-    return (
-        <h1>Canal</h1>
-    )
-}
