3b4af13aba0a89a571136af33a375a208097bc08
FellipeCastro <fellipecastro27d@gmail.com>
2023-12-11 18:27:42 -0300

    aula 33 finalizada

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -2,41 +2,95 @@ import React, {useState} from 'react';
 
 import './App.css';
 
-import Tabela from './components/Tabela';
-import InputPeso from './components/InputPeso';
-import InputAltura from './components/InputAltura';
-import CalcularIMC from './components/CalcularIMC';
-import Resultado from './components/Resultado';
-
 export default function App() {
 
-  const [peso, setPeso] = useState()
-  const [altura, setAltura] = useState()
+  const [valorTela, setValorTela] = useState("")
   const [resultado, setResultado] = useState(0)
+  const [acumulador, setAcumulador] = useState(0)
+  const [operado, setOperado] = useState(false)
 
-  return (
+  // Componentes
+  const tela = (valor, res) => {
+    return (
+      <div style={cssTela}>
+        <span style={cssTelaOperacao}>{valor}</span>
+        <span style={cssTelaResultado}>{res}</span>
+      </div>
+    )
+  }
+
+  const btn = (label, onClick) => {
+    return (
+      <button style={cssBtn} onClick={onclick}>
+        {label}
+      </button>
+    )
+  }
+
+  // Funções
+  const addDigitoTela = (d) => {
+    if ((d == "+" || d == "-" || d == "*" || d == "/") && operado) {
+      console.log("+-*/")
+      setOperado(false)
+      setValorTela(resultado + d)
+      return
+    } 
+
+    if (operado) {
+      setValorTela(d)
+      setOperado(false)
+      return
+    }
+
+    const valorDigitadoTela = valorTela + d
+    setValorTela(valorDigitadoTela)
+  }
+
+  const limparMemoria = () => {
+    setOperado(false)
+    setValorTela('')
+    setResultado(0)
+    setAcumulador(0)
+    return
+  }
+
+  // Estilos
+  const cssTela = {
+    display:'flex',
+    padding: '0 20px',
+    justifyContent: 'center',
+    alignItems: 'flex-start',
+    backgroundColor: '#444',
+    flexDirection: 'column',
+    width: '260px'
+  }
+  
+  const cssTelaOperacao = {
+    fontSize: '25px',
+    color: '#fff',
+    height: '20px',
+  }
+  
+  const cssTelaResultado = {
+    fontSize: '50px',
+    color: '#fff',
+  }
+
+  const cssBtn = {
+    fontSize: '30px',
+    height: '75px',
+    width: '75px',
+    padding: '20px',
+    backgroundColor: '#000',
+    color: '#fff',
+    borderColor: '#000',
+    textAlign: 'center',
+    outline: 'none'
+  }
+  
+  return (    
     <>
-      <InputPeso
-        peso={peso}
-        setPeso={setPeso}
-      />
 
-      <InputAltura
-        altura={altura}
-        setAltura={setAltura}
-      />
-
-      <CalcularIMC
-        peso={peso}
-        altura={altura}
-        setResultado={setResultado}
-      />
-
-      <Resultado
-        resultado={resultado}
-      />
-
-      <Tabela/>
     </>
   );
 }
